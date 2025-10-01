9ba1e6a278073ce984af5f527464240b1afb185d
tammirr <t9472683@gmail.com>
2025-10-01 11:16:20 +0700

    Add files via upload

---
+++ Tipe Data Numerik.py
@@ -0,0 +1,16 @@
+panjang = 15
+lebar = 18
+luas = panjang * lebar 
+print(panjang, '*', lebar, '=', luas)
+print("Tipe dari variabel panjang:", type(panjang))
+print("Tipe dari variabel lebar:", type(lebar))
+print("Tipe dari variabel luas:", type(luas))
+
+
+a = 10j
+b = 20j
+c = a + b 
+print(a, '+', b, '=', c)
+print("Tipe dari a:", type(a))
+print("Tipe dari b:", type(b))
+print("Tipe dari c:", type(c))
