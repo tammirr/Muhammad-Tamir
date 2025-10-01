ccef218d59ca0cba4c9ff4dd90c835f779137cdf
tammirr <t9472683@gmail.com>
2025-10-01 11:15:49 +0700

    Add files via upload

---
+++ Data Diri.py
@@ -0,0 +1,4 @@
+print("Nama saya : Muhammad Tamir")
+print("TTL : Bedengung, 17 Juli 2006")
+print("Sekarang saya tinggal di desa Cengkongabang")
+print("Hobi saya adalah bermain sepak bola, futsal dan memancing")
