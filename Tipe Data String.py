0b2a295ea5d024ba98f0a10fa1566cc0115d5e08
tammirr <t9472683@gmail.com>
2025-10-01 11:16:39 +0700

    Add files via upload

---
+++ Tipe Data String.py
@@ -0,0 +1,18 @@
+nama_depan = "Muhammad"
+nama_belakang = 'Tamir'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '19'
+alamat = 'cengkong abang'
+kata_mutiara = "hidup cuma satu kali jadi jangan di sia-sia kan"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
+
+#perjumlahan dua data numerik
+print(5 + 5)
+#perjumlahan dua data string
+print('5' + '5')
